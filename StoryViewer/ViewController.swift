afe993188023dded2b31d5945d7e014ac118be9e
Julien DAUPHANT <judauphant@gmail.com>
2015-03-17 18:25:01 +0100

    Initial Commit

---
+++ StoryViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoryViewer
+//
+//  Created by Julien DAUPHANT on 17/03/15.
+//  Copyright (c) 2015 Siz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
