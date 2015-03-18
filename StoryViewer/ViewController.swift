dc99ea2c1a0a1751c8538b8df71a2bf04baf3a42
Julien DAUPHANT <judauphant@gmail.com>
2015-03-18 12:26:51 +0100

    Play one video

--- StoryViewer/ViewController.swift
+++ StoryViewer/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
 
+    var moviePlayer : MPMoviePlayerController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        playVideo()
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func playVideo() {
+       let url = NSURL(string: "http://fun.siz.io/stories/1425445499218efb0f23ec97/2.mp4")
+       moviePlayer = MPMoviePlayerController(contentURL: url)
+       if let player = moviePlayer {
+            player.view.frame = CGRect(x: 0, y: 0, width: 352, height: 226)
+            self.view.addSubview(player.view)
+            player.controlStyle = MPMovieControlStyle.None
+            player.repeatMode = MPMovieRepeatMode.One
+            player.play()
+        }
+    }
 }
 
