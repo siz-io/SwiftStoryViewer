7b76ed2039f895bca24fd8026317938e7e5dfc29
Julien DAUPHANT <judauphant@gmail.com>
2015-04-01 18:46:02 +0200

    Change Story from class to struct

--- StoryViewer/Story.swift
+++ StoryViewer/Story.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct Story
+class Story
 {
     let id: String
     
@@ -21,10 +21,16 @@ struct Story
         }
     }
     
-    static func getRandomStory() -> Story {
-        let randomIndex = Int(arc4random_uniform(UInt32(storyIds.count)))
-        return Story(id: storyIds[randomIndex])
+    init(id: String) {
+        self.id = id
     }
     
-    private static let storyIds = ["1426589791593b2493fba815","1427469323995ed5cd418e9f","14274674523774c0ce58fca8","142746486623048953be9ce8","1427463698833a8039eef668","14274635585026374000e5fa","14274587736024eea248c438","1427457701047780790e00b1","1427456107646465bc0c8c5d","14274553480930af2fe62e48","142745454635644e9cb4236b"]
+    private struct Database {
+        static let storyIds = ["1426589791593b2493fba815","1427469323995ed5cd418e9f","14274674523774c0ce58fca8","142746486623048953be9ce8","1427463698833a8039eef668","14274635585026374000e5fa","14274587736024eea248c438","1427457701047780790e00b1","1427456107646465bc0c8c5d","14274553480930af2fe62e48","142745454635644e9cb4236b"]
+    }
+    
+    class func getRandomStory() -> Story {
+        let randomIndex = Int(arc4random_uniform(UInt32(Database.storyIds.count)))
+        return Story(id: Database.storyIds[randomIndex])
+    }
 }
