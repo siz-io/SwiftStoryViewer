b5dc9c42e613fadf326f614c8371aa132f6cfc4a
Julien DAUPHANT <judauphant@gmail.com>
2015-04-03 18:27:13 +0200

    Improve Story model

--- StoryViewer/Story.swift
+++ StoryViewer/Story.swift
@@ -10,7 +10,55 @@ import Foundation
 
 class Story: Printable
 {
+    struct Format
+    {
+        let type: String
+        let href: String
+        
+        init?(fromNSDictionary dict: NSDictionary)
+        {
+            if let type = dict["type"] as? String {
+                self.type = type
+                if let href = dict["href"] as? String {
+                    self.href = href
+                    return
+                }
+            }
+            return nil
+        }
+    }
+    
+    struct Box
+    {
+        let height: Int
+        let width: Int
+        let formats: [Format]
+        
+        init?(fromNSDictionary dict: NSDictionary)
+        {
+            if let height = dict["height"] as? Int {
+                self.height = height
+                if let width = dict["width"] as? Int {
+                    self.width = width
+                    if let formatDicts = dict["formats"] as? [NSDictionary] {
+                        var formats = [Format]()
+                        for formatDict in formatDicts {
+                            if let format = Format(fromNSDictionary: formatDict) {
+                                formats.append(format)
+                            }
+                        }
+                        self.formats = formats
+                        return
+                    }
+                }
+            }
+            return nil
+        }
+    }
+    
     let id: String
+    let boxes: [Box]
+    let slug: String
     
     var description: String {
         get {
@@ -18,36 +66,38 @@ class Story: Printable
         }
     }
     
-    var boxes: [String] {
-        get {
-            return ["http://fun.siz.io/stories/\(id)/0.mp4",
-                "http://fun.siz.io/stories/\(id)/1.mp4",
-                "http://fun.siz.io/stories/\(id)/2.mp4",
-                "http://fun.siz.io/stories/\(id)/3.mp4"]
-        }
-    }
-    
-    init(id: String) {
-        self.id = id
-    }
-    
     init?(fromNSDictionary dict: NSDictionary)
     {
         if let id = dict["id"] as? String {
             self.id = id
-        } else {
-            self.id = ""
-            return nil
+            if let slug = dict["slug"] as? String {
+                self.slug = slug
+                if let boxesDict = dict["boxes"] as? [NSDictionary] {
+                    var boxes = [Box]()
+                    for boxDict in boxesDict {
+                        if let box = Box(fromNSDictionary: boxDict) {
+                            boxes.append(box)
+                        }
+                    }
+                    self.boxes = boxes
+                    return
+                }
+            }
         }
+        self.id = ""
+        self.slug = ""
+        self.boxes = [Box]()
+        return nil
     }
-        
+    
     struct Database {
         static var currentStories = [Story]()
     }
     
     class func getNextStory(onSuccess: (Story) -> Void) {
         if !Database.currentStories.isEmpty {
-            onSuccess(Story(id: Database.currentStories.removeAtIndex(0).id))
+            let nextStory = Database.currentStories.removeAtIndex(0)
+            onSuccess(nextStory)
         } else {
             getStories() {
                 (stories) in
--- StoryViewer/StoryViewController.swift
+++ StoryViewer/StoryViewController.swift
@@ -21,7 +21,12 @@ class StoryViewController: UIViewController {
             if let model = story {
                 for (i,controller) in videoViewControllers {
                     let currentBox = i%model.boxes.count
-                    controller.sourceURL = model.boxes[currentBox]
+                    for format in model.boxes[currentBox].formats {
+                        if format.type == "mp4" {
+                            controller.sourceURL = format.href
+                            break
+                        }
+                    }
                 }
             }
         }
@@ -34,25 +39,6 @@ class StoryViewController: UIViewController {
         }
     }
     
-    func changeTo1x4()
-    {
-        if var newController = storyboard?.instantiateViewControllerWithIdentifier("Story View 4x1") as? StoryViewController {
-            newController.view.frame = parentViewController!.view.bounds
-            newController.story = self.story?
-            parentViewController?.addChildViewController(newController)
-            willMoveToParentViewController(nil)
-            parentViewController?.transitionFromViewController(self,
-                toViewController: newController,
-                duration: 0,
-                options: UIViewAnimationOptions.TransitionNone,
-                animations: {},
-                completion: { (finished: Bool) -> Void in
-                    self.removeFromParentViewController()
-                    newController.didMoveToParentViewController(self)
-                }
-            )
-        }
-    }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if let identifier = segue.identifier {
